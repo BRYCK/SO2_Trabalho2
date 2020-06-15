d758f492dfb6bbcc4de47b57410c2bd5dd67b44b
João da Conceição <32332929+BRYCK@users.noreply.github.com>
2020-06-15 21:56:11 +0100

    fuck

--- Trabalho2/src/main/java/so2/Trabalho2/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package SO2.SO2_Trabalho2;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoginRepository extends CrudRepository<Login, String>{
-    
-}
---
+++ src/test/java/SO2/SO2_Trabalho2/So2Trabalho2ApplicationTests.java
@@ -0,0 +1,13 @@
+package SO2.SO2_Trabalho2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So2Trabalho2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
