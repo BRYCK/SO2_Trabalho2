1693257b084ee9bf32d2d8d0826aa4a06cb589a8
João da Conceição <32332929+BRYCK@users.noreply.github.com>
2020-06-15 20:40:53 +0100

    Cagar Nisto2

---
+++ Trabalho2/src/main/java/so2/Trabalho2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package so2.Trabalho2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Trabalho2Application.class);
+	}
+
+}
