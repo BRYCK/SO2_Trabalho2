24f4e7833468670e13dd8187ca13d3126a860868
João da Conceição <32332929+BRYCK@users.noreply.github.com>
2020-06-18 22:59:06 +0100

    role

---
+++ src/main/java/SO2/SO2_Trabalho2/model/Role.java
@@ -0,0 +1,41 @@
+package SO2.SO2_Trabalho2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String role;
+
+    protected Role() {
+
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
--- src/main/java/SO2/SO2_Trabalho2/services/LojasService.java
+++ src/main/java/SO2/SO2_Trabalho2/services/LojasService.java
@@ -6,14 +6,14 @@ import SO2.SO2_Trabalho2.repository.LojasRepository;
 import java.util.List;
 
 public class LojasService {
-    
+
     @Autowired
     LojasRepository lojaRepo;
 
-    public List<Lojas> getAllLojas(){
-        List<Lojas> lojas=new List<>();
-        lojaRepo.findAll().forEach(lojas :: add);
+    public List<Lojas> getAllLojas() {
+        List<Lojas> lojas = new List<>();
+        lojaRepo.findAll().forEach(lojas::add);
         return lojas;
     }
-    
+
 }
