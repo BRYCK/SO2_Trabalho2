6b6207578322cbc8cac31204019abe05f76e86ea
Diogo <diogoraf8@gmail.com>
2020-06-20 16:25:56 +0100

    query??

--- src/main/java/SO2/SO2_Trabalho2/repository/LojaRepository.java
+++ src/main/java/SO2/SO2_Trabalho2/repository/LojaRepository.java
@@ -1,11 +1,15 @@
 package SO2.SO2_Trabalho2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import SO2.SO2_Trabalho2.model.Loja;
+import SO2.SO2_Trabalho2.model.Registo;
 
 @Repository
 public interface LojaRepository extends JpaRepository<Loja, Long> {
-
+    @Query("select Registo r from Loja l where r.data>= :currTime IN l.registos")
+    public List<Registo> findRegistoLastHour(long currTime);
 }
